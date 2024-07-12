5504b653addca942234e3be155b21819e52a3c17
rogervalentim <valentim.roger33@gmail.com>
2024-07-12 19:42:41 -0300

    feat: add accordion component

---
+++ src/components/accordion-component.tsx
@@ -0,0 +1,32 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography
+} from "@mui/material";
+import { ChevronUp } from "lucide-react";
+
+export function AccordionComponent() {
+  return (
+    <Accordion defaultExpanded sx={{ boxShadow: "none", padding: "1.25rem" }}>
+      <AccordionSummary
+        expandIcon={<ChevronUp color="#4d4d4d" />}
+        aria-controls="panel1-content"
+        id="panel1-header"
+        sx={{
+          paddingLeft: "0px",
+          paddingRight: "0px"
+        }}
+      >
+        <Typography fontWeight={"800"} fontSize={"1.00rem"} color={"#4D4D4D"}>
+          Como funciona?
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea nemo nihil
+        numquam ipsa blanditiis, et officia cumque iste quasi! Fugiat unde at
+        nemo accusamus vel modi magni possimus quis placeat?
+      </AccordionDetails>
+    </Accordion>
+  );
+}
